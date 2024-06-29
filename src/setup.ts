355101fd3730fe4b5a509bab0c94c8eebd13a884
Simone Parente <parentesimone.01@gmail.com>
2024-06-29 13:12:03 +0200

    Update setup.ts

--- src/setup.ts
+++ src/setup.ts
@@ -1,3 +1,9 @@
 import dotenv from "dotenv";
 
 dotenv.config();
+
+if (!process.env.SECRET_KEY) {
+  throw new Error("SECRET_KEY must be defined in environment variables");
+}
+
+export const SECRET_KEY: string = process.env.SECRET_KEY;
