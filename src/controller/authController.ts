8a334d294ae549e24742682f4c9a42b0c6aacfb9
Simone Parente <parentesimone.01@gmail.com>
2024-06-29 13:10:34 +0200

    added authController to manage routes

---
+++ src/controller/authController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express";
+import User, { register, login } from '../model/User';
+import Token, {invalidateToken} from "../model/Token";
+import { generateToken, validateToken} from '../security/authorization';
+
+
+export async function authRegister(req: Request, res: Response){
+    try{
+        const { username, email, password } = req.body;
+        let checkUsername = await User.findOne({ where: { username } });
+        let checkEmail = await User.findOne({ where: { email } });
+        if (!checkUsername) {
+            if (!checkEmail) {
+                await register(username, email, password);
+                res.status(201).send("User registered successfully");
+            } else {
+                res.status(400).send("Email already in use");
+            }
+        } else {
+            res.status(400).send("Username already in use");
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export async function authLogin(req: Request, res: Response){
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { email } });
+        if (user) {
+            const successfulLogin = await login(user, password);
+
+            if (successfulLogin) {
+                const token = generateToken(user);
+                res.set({
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                });
+                res.status(200).json({ message: "Login successful"});
+
+            } else {
+                res.status(401).send("Invalid email or password");
+            }
+        } else {
+            res.status(401).send("Invalid email or password");
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).send("Internal server error");
+    }
+}
+
+export async function authLogout(req: Request, res: Response){
+    const header = req.get('Authorization');
+    if(!header) return res.status(401).send("Unauthorized");
+    if(await validateToken(header)){
+        const token = header.split(' ')[1];
+        await invalidateToken(token);
+        res.status(200).send("Logout successful");
+    } else{
+        res.status(401).send("Unauthorized");
+    }
+}
+
+
+export async function getUsers(req: Request, res: Response){
+    let users = await User.findAll();
+    let header = req.get('Authorization')
+    if(!header) return res.status(401).send("Unauthorized");
+    if(await validateToken(header)){
+        res.status(200).send(users);
+    } else{
+        res.status(401).send("Unauthorized");
+    }
+}
+
+export async function getTokens(req: Request, res: Response){
+    let tokens = await Token.findAll();
+    res.status(200).send(tokens);
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,145 +1,31 @@
 import { Router, Request, Response } from 'express';
-
-import User, { register, login } from './model/User';
-
-import Token from './model/Token';
-
-import { generateToken, generateToken1sec, generateToken5years, validToken,  } from './security/authorization';
+import { authRegister, authLogin, authLogout, getUsers, getTokens,  } from './controller/authController';
 
 
-import { invalidateToken } from './model/Token';
 
 
 const router: Router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-    res.status(201).send("Endpoints available: <b>/api/register</b>, <b>/api/login</b>");
+    res.status(201)
+    .send("Endpoints available:" +
+          "POST <b>/api/register</b>, <b>/api/login</b>, <b>/api/logout</b>,"
+        + "GET  <i>/api/users</i>, <b>/api/Tokens</b>");
 });
 
-router.post("/api/register", async (req: Request, res: Response) => {
-    try{
-        const { username, email, password } = req.body;
-        let checkUsername = await User.findOne({ where: { username } });
-        let checkEmail = await User.findOne({ where: { email } });
-        if (!checkUsername) {
-            if (!checkEmail) {
-                await register(username, email, password);
-                res.status(201).send("User registered successfully");
-            } else {
-                res.status(400).send("Email already in use");
-            }
-        } else {
-            res.status(400).send("Username already in use");
-        }
-    } catch (e) {
-        console.log(e);
-    }
-});
+//-------------------POST-------------------
 
+router.post("/api/register", authRegister);
 
-router.post("/api/login", async (req : Request, res: Response) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ where: { email } });
-        if (user) {
-            const successfulLogin = await login(user, password);
-            if (successfulLogin) {
-                const token = generateToken(user);
+router.post("/api/login", authLogin);
 
-                res.set({
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                });
+router.post("/api/logout", authLogout);
 
-                res.status(200).json({ message: "Login successful"});
-            } else {
-                res.status(401).send("Invalid email or password");
-            }
-        } else {
-            res.status(401).send("Invalid email or password");
-        }
-    } catch (error) {
-        console.error('Error during login:', error);
-        res.status(500).send("Internal server error");
-    }
-});
+//-------------------GET-------------------
 
-router.post("/api/logout"), async (req: Request, res: Response) => {
-    const header = req.get('Authorization');
-    if(await validToken(header)){
-        invalidateToken(header);
-        res.status(200).send("Logout successful");
-    } else{
-        res.status(401).send("Unauthorized");
-    }
-}
+router.get("/api/users", getUsers);
 
-//-------------------DA ELIMINARE-------------------
-router.post("/api/login/1second", async (req : Request, res: Response) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ where: { email } });
-        if (user) {
-            const successfulLogin = await login(user, password);
-            if (successfulLogin) {
-                res.set({
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${generateToken1sec(user)}`
-                });
-                res.status(200).json({ message: "Login successful"});
-            } else {
-                res.status(401).send("Invalid email or password");
-            }
-        } else {
-            res.status(401).send("Invalid email or password");
-        }
-    } catch (error) {
-        console.error('Error during login:', error);
-        res.status(500).send("Internal server error");
-    }
-});
-
-router.post("/api/login/5years", async (req : Request, res: Response) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ where: { email } });
-        if (user) {
-            const successfulLogin = await login(user, password);
-            if (successfulLogin) {
-                res.set({
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${generateToken5years(user)}`
-                });
-                res.status(200).json({ message: "Login successful"});
-            } else {
-                res.status(401).send("Invalid email or password");
-            }
-        } else {
-            res.status(401).send("Invalid email or password");
-        }
-    } catch (error) {
-        console.error('Error during login:', error);
-        res.status(500).send("Internal server error");
-    }
-});
-
-
-//---------------------------FINE DA ELIMINARE-------------------
-
-router.get("/api/users",  async (req: Request, res: Response) => {
-        let users = await User.findAll();
-        let header = req.get('Authorization')
-        if(await validToken(header)){
-            res.status(200).send(users);
-        } else{
-            res.status(401).send("Unauthorized");
-        }
-});
-
-router.get("/api/Tokens", async (req: Request, res: Response) => {
-    let tokens = await Token.findAll();
-    res.status(200).send(tokens);
-});
+router.get("/api/tokens", getTokens);
 
 
 export default router;
